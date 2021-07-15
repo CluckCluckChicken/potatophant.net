ad23674a1eac44117568c652049f77cc9f4738fd
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-07-15 17:40:27 +0100

    init plausible

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
-      { defer: 'defer', src: 'https://static.cloudflareinsights.com/beacon.min.js', 'data-cf-beacon': '{"token": "26df8b2184694c26b8a9e6071174deaf"}' }
+      { src: 'https://analytics.potatophant.net/js/plausible.js' }
     ]
   },
 
